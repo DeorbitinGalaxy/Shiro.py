b00086bab3851070dcdbc01064040e9ea5dc6a5f
StrawHatHacker <34351536+StrawHatHacker@users.noreply.github.com>
2018-05-10 19:40:58 +0100

    Add files via upload

---
+++ ShiroBot_Public/config.py
@@ -0,0 +1,11 @@
+prefix = ">"
+token = ""
+#======================================================
+loop_time = 240 #seconds #please use multples of 60
+#======================================================
+color_green 				= 0x00b300
+color_red					= 0xe60000
+color_yellow				= 0xcccc00
+color_blue					= 0x3333cc
+#======================================================
+bot_invite_link        		= "https://discordapp.com/oauth2/authorize?&client_id=444198945183367189&scope=bot&permissions=0"
