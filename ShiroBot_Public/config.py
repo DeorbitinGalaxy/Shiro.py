9547cfa82a1446c061c74f9b85ad6ffbb48ed07a
StrawHatHacker <34351536+StrawHatHacker@users.noreply.github.com>
2018-05-10 19:15:38 +0100

    Add files via upload

---
+++ ShiroBot_Public/config.py
@@ -0,0 +1,11 @@
+prefix = ">"
+token = "NDQ0MTk4OTQ1MTgzMzY3MTg5.DdYcqg.0Nnxt9nlbykuYbEgw9pK_a7XLWk"
+#======================================================
+loop_time = 240 #seconds #please use multples of 60
+#======================================================
+color_green 				= 0x00b300
+color_red					= 0xe60000
+color_yellow				= 0xcccc00
+color_blue					= 0x3333cc
+#======================================================
+bot_invite_link        		= "https://discordapp.com/oauth2/authorize?client_id=444198945183367189&permissions=0&redirect_uri=https%3A%2F%2Fgithub.com%2FStrawHatHacker%2FShiro&scope=bot"
