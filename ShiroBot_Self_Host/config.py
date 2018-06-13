b59a123633ebe41dd2e96b54ff5bb595406a37f1
StrawHatHacker <34351536+StrawHatHacker@users.noreply.github.com>
2018-06-13 15:26:12 +0300

    Update config.py

--- ShiroBot_Self_Host/config.py
+++ ShiroBot_Self_Host/config.py
@@ -1,27 +1,27 @@
 prefix    = ">"
-token     = "NDQ0MTk4OTQ1MTgzMzY3MTg5.DdYcqg.0Nnxt9nlbykuYbEgw9pK_a7XLWk"
-owner_ids =	["285495027411582977", "161600697564004352"] 
+token     = "k"
+owner_ids =	[] 
 #======================================================
 loop_time = 240 #seconds #please use multples of 60
 #======================================================
-bot_test_channel_id     	= "456421487772041237"
+bot_test_channel_id     	= "
 # bot will return errors here ^
 #======================================================
 member_join_message    		= True
-member_join_channel_id 		= "440510934012788736"
+member_join_channel_id 		= ""
 #======================================================
 member_join_log 			= True
 member_leave_log			= True
-member_log_channel_id 		= "440855729419714570"
+member_log_channel_id 		= ""
 #======================================================
 member_ban_log 				= True
 member_kick_log				= True
 member_unban_log			= True
-member_bankick_channel_id 	= "440511224640176128"
+member_bankick_channel_id 	= ""
 #======================================================
 message_delete_log 			= True
 message_edit_log			= True
-message_log_channel_id 		= "440511273738829845"
+message_log_channel_id 		= ""
 #======================================================
 color_green 				= 0x00b300
 color_red					= 0xe60000
